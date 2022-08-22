2de92a609cc8ac3351e5f9c56c8eab05b5717eac
ibd129 <1536479720@qq.com>
2022-08-22 14:23:50 +0800

    bug change

--- src/views/BlurryLoading.vue
+++ src/views/BlurryLoading.vue
@@ -14,6 +14,7 @@ export default {
     })
     // 数字增加
     function numAdd () {
+      data.opacity = 0
       if (data.num < 100) {
         data.num++
       }
@@ -28,14 +29,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .loading{
   width: 100%;
   height: 100vh;
-  background-image: url(http://pic1.win4000.com/wallpaper/2018-01-12/5a587e14cf639.jpg);
+  background-image: url(https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fphoto%2F2012-3-2%2Fenterdesk.com-B526ECADD33DBD367676A93E051BA1EC.jpg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1663740174&t=733737d9a98b94a834ef5b9248ad507b);
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
+  animation: 2s filterAn ease-in;
+  @keyframes filterAn {
+    from {filter:blur(15px)}
+    to {filter:blur(0px)}
+  }
 }
 .loading-text{
   color: white;
@@ -44,5 +50,7 @@ export default {
   top:50%;
   left: 50%;
   font-weight: bold;
+  opacity: v-bind('data.opacity');
+  transition: opacity 2s ease-in;
 }
 </style>
